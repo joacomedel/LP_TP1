fa49732da966f855d2db24354af1c32ba6934304
Iovaldi, Sebastian <sebaiova95@gmail.com>
2023-08-25 01:41:49 -0300

    corregido constructor

--- src/Ordenador.java
+++ src/Ordenador.java
@@ -1,9 +1,11 @@
 import java.util.concurrent.Callable;
 
 public class Ordenador<T> implements Callable {
+
     private AlgoritmoOrdenamiento algoritmo;
     private int[] arreglo;
-    public Ordenador (AlgoritmoOrdenamiento algoritmoOrdenamiento ,int[] arreglo){
+
+    public Ordenador (AlgoritmoOrdenamiento algoritmo ,int[] arreglo){
         this.algoritmo = algoritmo;
         this.arreglo = arreglo;
 
