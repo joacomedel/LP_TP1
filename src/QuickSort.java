38ca16d2d278b30f1763519a7c5e99e9cdd90fb4
joacomedel <joaquinmedel1234@gmail.com>
2023-08-30 19:10:04 -0300

    QuickSort

---
+++ src/QuickSort.java
@@ -0,0 +1,35 @@
+public class QuickSort<T extends Comparable<T>> implements AlgoritmoOrdenamiento<T> {
+    
+    public T[] ordenar(T[] arregloOriginal) {
+        
+        quickSort(arregloOriginal, 0, arregloOriginal.length - 1);
+        return arregloOriginal;
+
+    }
+    private <T extends Comparable<T>> void quickSort(T[] array, int left, int right) {
+        if (left < right) {
+            int pivotIndex = partition(array, left, right);
+            quickSort(array, left, pivotIndex - 1);
+            quickSort(array, pivotIndex + 1, right);
+        }
+    }
+    private <T extends Comparable<T>> int partition(T[] array, int left, int right) {
+        T pivot = array[right];
+        int i = left - 1;
+
+        for (int j = left; j < right; j++) {
+            if (array[j].compareTo(pivot) <= 0) {
+                i++;
+                swap(array, i, j);
+            }
+        }
+
+        swap(array, i + 1, right);
+        return i + 1;
+    }
+    private <T extends Comparable<T>> void swap(T[] array, int i, int j) {
+        T temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
