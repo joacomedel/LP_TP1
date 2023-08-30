f98b5f5ae3cf25926f501e24bf80997e9e58065c
Iovaldi, Sebastian <sebaiova95@gmail.com>
2023-08-30 18:39:26 -0300

    .

---
+++ src/ConcurrentSort.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class ConcurrentSort<T extends Comparable<T>> {
+    
+    private final ExecutorService executor = Executors.newFixedThreadPool(5);
+    private Collection<Callable<T[]>> callables;
+
+    public T[] sort(T[] array) throws InterruptedException, ExecutionException
+    {
+        callables = new ArrayList<>();
+
+        callables.add(new Ordenador<T>(new BubbleSort<T>(), array.clone()));
+
+        T[] result = executor.invokeAny(callables);
+        
+        executor.shutdown();
+
+        return result;
+    }
+
+}
