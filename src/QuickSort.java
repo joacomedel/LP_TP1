79de6bc491226c0ef2543e8fef6958b067d3a7e3
Iovaldi, Sebastian <sebaiova95@gmail.com>
2023-08-30 19:11:43 -0300

    quick sort prolijo

--- src/QuickSort.java
+++ src/QuickSort.java
@@ -1,19 +1,22 @@
 public class QuickSort<T extends Comparable<T>> implements AlgoritmoOrdenamiento<T> {
     
-    public T[] ordenar(T[] arregloOriginal) {
-        
+    public T[] ordenar(T[] arregloOriginal) 
+    {
         quickSort(arregloOriginal, 0, arregloOriginal.length - 1);
         return arregloOriginal;
-
     }
-    private <T extends Comparable<T>> void quickSort(T[] array, int left, int right) {
+
+    private void quickSort(T[] array, int left, int right) 
+    {
         if (left < right) {
             int pivotIndex = partition(array, left, right);
             quickSort(array, left, pivotIndex - 1);
             quickSort(array, pivotIndex + 1, right);
         }
     }
-    private <T extends Comparable<T>> int partition(T[] array, int left, int right) {
+
+    private int partition(T[] array, int left, int right) 
+    {
         T pivot = array[right];
         int i = left - 1;
 
@@ -23,11 +26,12 @@ public class QuickSort<T extends Comparable<T>> implements AlgoritmoOrdenamiento
                 swap(array, i, j);
             }
         }
-
         swap(array, i + 1, right);
         return i + 1;
     }
-    private <T extends Comparable<T>> void swap(T[] array, int i, int j) {
+
+    private void swap(T[] array, int i, int j) 
+    {
         T temp = array[i];
         array[i] = array[j];
         array[j] = temp;
